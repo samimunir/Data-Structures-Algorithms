c66e72ff37bd7215d9b896429cdeb1c2fb83b070
Sami Munir <smunir01@outlook.com>
2024-01-22 22:09:39 -0500

    adding SinglyLinkedlist object & constructors

---
+++ Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/SinglyLinkedlist.java
@@ -0,0 +1,19 @@
+public class SinglyLinkedlist {
+    private Node head;
+    private int totalElements;
+    
+    public SinglyLinkedlist() {
+        this.head = null;
+        this.totalElements = 0;
+    }
+
+    private void printDS() {
+        //
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        return output;
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -3,5 +3,6 @@ Sami Munir | January 22nd, 2024 | Rutgers University | smunir01@outlook.com
 ## Data Structures
 ### Linkedlists
 * Singly Linkedlist
+    * Java
 * Doubly Linkedlist
 ## Algorithms
