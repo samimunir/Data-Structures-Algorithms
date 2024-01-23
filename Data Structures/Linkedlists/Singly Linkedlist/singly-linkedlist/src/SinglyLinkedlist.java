c97a061ce092f65b6c3d459c3af52da059f6b9c2
Sami Munir <smunir01@outlook.com>
2024-01-22 22:14:49 -0500

    adding helper functions isEmpty(), printDS(), printDSStats()

--- Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/SinglyLinkedlist.java
+++ Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/SinglyLinkedlist.java
@@ -7,13 +7,31 @@ public class SinglyLinkedlist {
         this.totalElements = 0;
     }
 
-    private void printDS() {
-        //
+    private boolean isEmpty() {
+        if (this.head == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    @Override
-    public String toString() {
-        String output = "";
-        return output;
+    private void printDS() {
+        if (isEmpty()) {
+            System.out.println("HEAD -> (EMPTY) -> NULL");
+        } else {
+            Node currentNode = this.head;
+            System.out.print("HEAD");
+            while (currentNode != null) {
+                System.out.print(" -> " + currentNode.getData());
+                currentNode = currentNode.getNext();
+            }
+            System.out.println("NULL");
+        }
+    }
+
+    private void printDSStats() {
+        System.out.println("Singly Linkedlist");
+        printDS();
+        System.out.println("total elements: " + this.totalElements);
     }
 }
