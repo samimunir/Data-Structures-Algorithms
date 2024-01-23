041f62ea11eb2202cdecf0b5a811ce3f3795a421
Sami Munir <smunir01@outlook.com>
2024-01-22 22:05:51 -0500

    Node object complete

--- Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/Node.java
+++ Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/Node.java
@@ -13,4 +13,25 @@ public class Node {
         this.data = data;
         this.next = next;
     }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    @Override
+    public String toString() {
+        return "data: " + this.data + "\nnext.data: " + this.next.data;
+    }
 }
