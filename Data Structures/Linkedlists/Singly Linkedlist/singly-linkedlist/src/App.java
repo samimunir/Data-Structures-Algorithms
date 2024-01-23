2b9dbc949ab8bc7981570546fc2f2721c15f4c73
Sami Munir <smunir01@outlook.com>
2024-01-22 22:18:29 -0500

    testing class SinglyLinkedlist

--- Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/App.java
+++ Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Singly Linkedlist - Java");
+        System.out.println("------------------------");
+        SinglyLinkedlist singlyLinkedlist = new SinglyLinkedlist();
+        singlyLinkedlist.printDSStats();
     }
 }
--- Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/SinglyLinkedlist.java
+++ Data Structures/Linkedlists/Singly Linkedlist/singly-linkedlist/src/SinglyLinkedlist.java
@@ -5,6 +5,7 @@ public class SinglyLinkedlist {
     public SinglyLinkedlist() {
         this.head = null;
         this.totalElements = 0;
+        System.out.println("\nSingly Linkedlist successfully created.");
     }
 
     private boolean isEmpty() {
@@ -17,10 +18,10 @@ public class SinglyLinkedlist {
 
     private void printDS() {
         if (isEmpty()) {
-            System.out.println("HEAD -> (EMPTY) -> NULL");
+            System.out.println("\tHEAD -> (EMPTY) -> NULL");
         } else {
             Node currentNode = this.head;
-            System.out.print("HEAD");
+            System.out.print("\tHEAD");
             while (currentNode != null) {
                 System.out.print(" -> " + currentNode.getData());
                 currentNode = currentNode.getNext();
@@ -29,9 +30,9 @@ public class SinglyLinkedlist {
         }
     }
 
-    private void printDSStats() {
-        System.out.println("Singly Linkedlist");
+    public void printDSStats() {
+        System.out.println("Singly Linkedlist DS Stats...");
         printDS();
-        System.out.println("total elements: " + this.totalElements);
+        System.out.println("\t\ttotal elements: " + this.totalElements);
     }
 }
