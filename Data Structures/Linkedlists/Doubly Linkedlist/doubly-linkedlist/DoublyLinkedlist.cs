6c47a6054a5c523609f0f225a72ad93b8a1e2bbc
Sami Munir <smunir01@outlook.com>
2024-01-25 03:27:14 -0500

    adding utility functions

--- Data Structures/Linkedlists/Doubly Linkedlist/doubly-linkedlist/DoublyLinkedlist.cs
+++ Data Structures/Linkedlists/Doubly Linkedlist/doubly-linkedlist/DoublyLinkedlist.cs
@@ -9,4 +9,32 @@ public class DoublyLinkedlist {
         this.totalElements = 0;
         Console.WriteLine("\nDoubly linkedlist successfully created.");
     }
+
+    private Boolean IsEmpty() {
+        if (this.head == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void PrintDS() {
+        if (IsEmpty()) {
+            Console.WriteLine("\tHEAD <-> (EMPTY) <-> NULL");
+        } else {
+            Node currentNode = this.head;
+            Console.Write("\tHEAD");
+            while (currentNode != null) {
+                Console.Write(" <-> " + currentNode.GetData());
+                currentNode = currentNode.GetNext();
+            }
+            Console.WriteLine(" <-> NULL");
+        }
+    }
+
+    public void PrintDSStats() {
+        Console.WriteLine("Doubly Linkedlist DS Stats...");
+        PrintDS();
+        Console.WriteLine("\t\ttotalElements: " + this.totalElements);
+    }
 }
