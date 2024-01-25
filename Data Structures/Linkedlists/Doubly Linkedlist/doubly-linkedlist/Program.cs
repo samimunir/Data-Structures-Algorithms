7f345dda7b83e7e930aa662d3040c03fc8bf9643
Sami Munir <smunir01@outlook.com>
2024-01-25 03:13:23 -0500

    adding data structure: doubly linkedlist - C#

---
+++ Data Structures/Linkedlists/Doubly Linkedlist/doubly-linkedlist/Program.cs
@@ -0,0 +1,6 @@
+﻿public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Doubly Linkedlist - C#");
+        Console.WriteLine("----------------------");
+    }
+}
