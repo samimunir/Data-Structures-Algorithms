c98d1d26740e360fc0745ff79a7455410e03c64b
Sami Munir <smunir01@outlook.com>
2024-01-25 03:22:38 -0500

    adding DoublyLinkedlist object + state/constructor

---
+++ Data Structures/Linkedlists/Doubly Linkedlist/doubly-linkedlist/DoublyLinkedlist.cs
@@ -0,0 +1,12 @@
+public class DoublyLinkedlist {
+    private Node head;
+    private Node tail;
+    private int totalElements;
+
+    public DoublyLinkedlist() {
+        this.head = null;
+        this.tail = null;
+        this.totalElements = 0;
+        Console.WriteLine("\nDoubly linkedlist successfully created.");
+    }
+}
