83bb88d2760e328193acabe936315e9317ba248f
Sami Munir <smunir01@outlook.com>
2024-02-06 07:17:09 -0500

    adding TreeNode structure & function createNode()

--- Data Structures/Trees/Binary Tree/binary-tree/source/binaryTree.c
+++ Data Structures/Trees/Binary Tree/binary-tree/source/binaryTree.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//
+struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int data) {
+    struct TreeNode *newNode = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+    if (newNode == NULL) {
+        printf("\n--<ERROR>-- Memory allocation failure!\n");
+        exit(1);
+    }
+    newNode -> data = data;
+    newNode -> left = NULL;
+    newNode -> right = NULL;
+    return newNode;
+}
 
 int main(int argc, char* argv[]) {
     printf("Binary Tree - C\n");
     printf("---------------\n");
-    
+
     return EXIT_SUCCESS;
 }
